86669cd4810eab0f1f2894a4f8335f76a1aeaa00
Md. Shariful Islam <abinsu7@gmail.com>
2022-12-07 17:17:09 +0600

    Entities are added

---
+++ cse499B/TicketBookingSystem/TicketBookingSystem.Infrastructure/Entities/Customer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TicketBookingSystem.Entities
+{
+    public class Customer : IEntity<int>
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Address { get; set; }
+    }
+}
---
+++ cse499B/TicketBookingSystem/TicketBookingSystem.Infrastructure/Entities/Ticket.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using TicketBookingSystem.Infrastructure.Entities;
+
+namespace TicketBookingSystem.Booking.Entities
+{
+    public class Ticket : IEntity<int>
+
+    {
+        public int Id { get; set; }
+        public int CustomerId { get; set; }
+        public string Destination { get; set; }
+        public float TicketFee { get; set; }
+    }
+}
