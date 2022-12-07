6066c41621ca1e4b1edd6c5ef1eeb1a4460a2819
Md. Shariful Islam <abinsu7@gmail.com>
2022-12-07 16:18:51 +0600

    Repository and UnitOfWork default classes are added in project.
    To handle data transmission web-page to database and database to web-page Helping DataTablesAjaxRequestModel.cs class is added in project

---
+++ cse499B/TicketBookingSystem/TicketBookingSystem.Infrastructure/UnitOfWorks/IUnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicketBookingSystem.Infrastructure.UnitOfWorks
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        void Save();
+    }
+}
