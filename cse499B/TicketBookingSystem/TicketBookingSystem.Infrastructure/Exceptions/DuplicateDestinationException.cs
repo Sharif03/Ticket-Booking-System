80b8cba7e89549456cf0ed2ca60aa06bfececee8
Md. Shariful Islam <abinsu7@gmail.com>
2022-12-07 17:57:10 +0600

    Two customized exception class is added, for error handling

---
+++ cse499B/TicketBookingSystem/TicketBookingSystem.Infrastructure/Exceptions/DuplicateDestinationException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace TicketBookingSystem.Infrastructure.Exceptions
+{
+    public class DuplicateDestinationException : Exception
+    {
+        public DuplicateDestinationException(string message) : base(message)
+        {
+        }
+    }
+}
